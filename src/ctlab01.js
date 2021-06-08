747737a3b657042b0c0fb5d5fbaa22f4381c9677
Tis Lais <tistististis@gmail.com>
2021-06-08 11:03:30 -0700

    add fetchQuote function with munge

--- src/ctlab01.js
+++ src/ctlab01.js
@@ -1,3 +1,5 @@
+import fetch from 'node-fetch';
+
 export function getName({ name }) {
   return name;
 }
@@ -10,3 +12,13 @@ export function capitalizeAndFilter(arr) {
   const newArr = arr.filter(string => !string.match(/^[Ff]/));
   return newArr.map(string => string.toUpperCase());
 }
+
+export async function fetchQuote() {
+  return fetch('http://futuramaapi.herokuapp.com/api/quotes')
+    .then(response => response.json())
+    .then(data => ({
+      name: data[0].character,
+      text: data[0].quote,
+      image: data[0].image
+    }));
+} 
