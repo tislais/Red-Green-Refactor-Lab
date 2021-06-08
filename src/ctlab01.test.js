b3186b3f619dec20df92d0214413ed378f161c4f
Tis Lais <tistististis@gmail.com>
2021-06-08 09:00:07 -0700

    add copyAndPush test

--- src/ctlab01.test.js
+++ src/ctlab01.test.js
@@ -1,6 +1,6 @@
 import getName from './ctlab01';
 
-describe('Red Green Refactor', () => {
+describe('getName function', () => {
   it('gets the name spot from an object', () => {
     const spot = { name: 'spot', age: 5, weight: '20 lbs' };
     expect(getName(spot)).toEqual('spot');
@@ -9,5 +9,12 @@ describe('Red Green Refactor', () => {
   it('gets the name Aang from an object', () => {
     const character = { _id: '5cf5679a915ecad153ab68c9', name: 'Aang' };
     expect(getName(character)).toEqual('Aang');
+  });
+});
+
+describe('copyAndPush function', () => {
+  it('that returns a new array with all the items in the original array and a new item pushed to the end', () => {
+    const numbers = [1, 2, 3];
+    expect(copyAndPush(numbers, 4)).toEqual([1, 2, 3, 4]);
   });
 });
